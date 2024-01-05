0c102fd2bdbfbf2e3f68fae9cb8f38d011814d9c
AshwinKumarRP <154827481+AshwinKumarRP@users.noreply.github.com>
2024-01-05 16:47:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Define variables
-IMAGE_NAME="dev"
-TAG="latest"
-DOCKERFILE="Dockerfile"  # Your Dockerfile name
 
 # Build the Docker image
-docker build -t "${IMAGE_NAME}:${TAG}" .
+docker build -t ashwin .
 
