6005de7c7ad07ed041034fdbc977b206284bae34
AshwinKumarRP <154827481+AshwinKumarRP@users.noreply.github.com>
2024-01-05 16:45:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,6 @@
 # Log in to Docker Hub
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD 
 
-# Tag the local image
-docker tag dev:latest ashwin055
-# Push the image to Docker Hub
-docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}"
-
-# Log out from Docker Hub
+docker-compose -up -d
 
 
