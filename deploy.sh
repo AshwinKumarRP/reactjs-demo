66093a5ba04549de246cdd29926e998fd8644e96
AshwinKumarRP <154827481+AshwinKumarRP@users.noreply.github.com>
2024-01-05 16:01:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-# Variables
-DOCKER_USERNAME="ashwin055"
-DOCKER_PASSWORD="dckr_pat_06USN1JjYRglmDIHW_OtG4BcVg0"
-IMAGE_NAME="dev"
-TAG="latest"
-
 # Log in to Docker Hub
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD 
 
 # Tag the local image
-docker tag "${IMAGE_NAME}:${TAG}" "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}"
-
+docker tag dev:latest ashwin055
 # Push the image to Docker Hub
 docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}"
 
 # Log out from Docker Hub
-docker logout
+
 
